21b8454d4039860fc209b0225bd5a9d02be12349
RudraGFG <rudra.makhija@geeksforgeeks.org>
2025-07-20 20:00:50 +0530

    Create printAntiDiagonal.cpp

---
+++ 19July-2D-Array/printAntiDiagonal.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+    vector<vector<int>> mat = {{10, 20, 30},
+        {40, 50, 60},
+        {70, 80, 90}
+    };
+    
+    int n = mat.size();
+    
+    // using 2 Loops
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(i + j == n-1){
+                cout << mat[i][j] << " ";
+            }
+        }
+    }
+    
+    cout << endl;
+    
+    // using 1 Loop
+    for(int i=0; i<n; i++){
+        cout << mat[i][n-i-1] << " ";
+    }
+    
+    return 0;
+}
