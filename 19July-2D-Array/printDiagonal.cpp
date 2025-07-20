8cbb6cd16244ee6ea070dd7e439911bdf1e4cae2
RudraGFG <rudra.makhija@geeksforgeeks.org>
2025-07-20 20:01:19 +0530

    Update printDiagonal.cpp

--- 19July-2D-Array/printDiagonal.cpp
+++ 19July-2D-Array/printDiagonal.cpp
@@ -11,8 +11,8 @@ int main() {
     int n = mat.size();
     
     // using 2 Loops
-    for(int j=0; j<n; j++){
-        for(int i=0; i<n; i++){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
             if(i == j){
                 cout << mat[i][j] << " ";
             }
